b236de40b1245cf70d74cba40eaada4c0dec232c
Moises Guilherme <moisescomp@gmail.com>
2021-07-21 19:16:18 -0300

    Lesson, Content, Task

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Content.java
@@ -0,0 +1,43 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_content")
+public class Content extends Lesson {
+	private static final long serialVersionUID = 1L;
+	
+	private String textContent;
+	private String videoUri;
+	
+	public Content() {
+		
+	}
+
+	public Content(Long id, String title, Integer position, Section section, String textContent, String videoUri) {
+		super(id, title, position, section);
+		this.textContent = textContent;
+		this.videoUri = videoUri;
+	}
+
+	public String getTextContent() {
+		return textContent;
+	}
+
+	public void setTextContent(String textContent) {
+		this.textContent = textContent;
+	}
+
+	public String getVideoUri() {
+		return videoUri;
+	}
+
+	public void setVideoUri(String videoUri) {
+		this.videoUri = videoUri;
+	}
+	
+	// Não tem o equals e o hashcode. Pois como ele herda do content, ele já testa por id. que tem nele. 
+	
+
+}
