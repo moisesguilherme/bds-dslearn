ab6e9cbd5c1ab87a2c2ca48c5c31ab621e960b48
Moises Guilherme <moisescomp@gmail.com>
2021-07-29 20:36:57 -0300

    Custom content for current user

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
@@ -1,11 +1,19 @@
 package com.devsuperior.dslearnbds.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.devsuperior.dslearnbds.entities.Course;
-import com.devsuperior.dslearnbds.entities.Lesson;
 import com.devsuperior.dslearnbds.entities.Notification;
+import com.devsuperior.dslearnbds.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
-					
+	
+	//JPQL
+	@Query("SELECT obj FROM Notification obj WHERE "
+			+ "(obj.user = :user) AND "
+			+ "(:unreadOnly = false OR obj.read = false) "
+			+ "ORDER BY obj.moment DESC")			
+	Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 }
